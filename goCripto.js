07cc7b5bb04857003fc54c1708cf7399602d08f4
MatiasDAndrea <105309938+MatiasDAndrea@users.noreply.github.com>
2022-06-16 19:25:17 -0300

    Main Structure

---
+++ goCripto.js
@@ -0,0 +1,62 @@
+
+/*
+    getAccount() se encarga de obtener de Main.html las cuentas
+    existentes del usuario.
+*/
+
+function init(){
+
+    /*
+    init() se encarga de inicializar los datos relevantes 
+    de la HomePage
+    */
+    fetch('Main.html')
+        .then(response=> response.text())
+        .then(text=> {
+
+            document.getElementById("HomeHTML").innerHTML = text
+            document.getElementById("")
+        });
+    
+
+}
+
+let HomeValues
+fetch('Main.html')
+.then(response=> response.text())
+.then(text=> HomeValues=text);
+
+let accounts = document.getElementsByClassName("AccountNumber")
+console.log(HomeValues)
+
+
+
+
+function asd(){
+
+    let endpoint = "https://api.binance.com/api/v3/ticker/price"
+    fetch(endpoint)
+        .then(el=>el.json())
+        .then(data=>plotValues(data))
+
+}
+
+function plotValues(data){
+    
+    /* 
+    plotValues(data) se encarga de modificar el codigo HTML
+    y plotear el valor de las criptomonedas mas relevantes.
+    */
+   let body=""
+   let regex = /USDT\b/
+
+   data.forEach(el=>{
+        if (regex.test(el.symbol) && el.price >=10){
+            body += `<tr><td>${el.symbol}</td><td>${el.price}</td></tr>`
+        }
+   })
+   
+   document.getElementById("CriptoValues").innerHTML = body
+}
+
+init()
