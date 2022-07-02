4e5a5df5770b102d9dec1dee862945f66b15cc4d
MatiasDAndrea <105309938+MatiasDAndrea@users.noreply.github.com>
2022-07-02 01:00:30 -0300

    Detalles Funciones Inicio-Deteccion de errores-Filtrado (Status:Done)

--- Sprint-4/listado_cheques.py
+++ Sprint-4/listado_cheques.py
@@ -1,69 +1,137 @@
 import sys
-import datetime
-sys.argv[1::] = ["text.txt","40134552","Pantalla","Emitido","53"]
+from datetime import datetime
+import getopt
+import re
 
-def search_check(lista, dni, types, status, date):
 
-    #search_check recibe una lista donde cada elemento es un reglon del archivo csv.
-    #devuelve una lista con los cheques filtrados.
+def search_check(lista, diccionario):
 
-    #Genero mi variable de output
-    output_list = []
-    status = False if status=="" else status
-    date = False if date=="" else date
+    #-------------------------------------------------------------------------
+    # search_check() se encarga de filtrar cheques segun los parametros
+    #    que ingreso el usuario.
+    #
+    # Input:
+    #   lista: Contiene todas las lineas del archivo leido.
+    #   diccionario: contiene todos los campos ingresados por el usuario.
+    #
+    # Ouput: 
+    #   cheques_filtrados: Lista conteniendo cada uno de los cheques filtrados.
+    #--------------------------------------------------------------------------
+
+    cheques_filtrados = []
+    dni = diccionario["dni"]
+    estado = diccionario.get("estado",False)
+    fecha_desde = diccionario.get("fecha_desde",False)
+    fecha_hasta = diccionario.get("fecha_hasta",False)
 
     for x in lista:
 
         x = x.replace("\n","").split(",")
-        if dni in x and types in x and (not status or (status and status in x)):
+        fecha_lista  = float(x[6])
+        dni_lista    = x[8]
 
-            output_list.append(x)
+        filtro_dni = dni == dni_lista
+        filtro_fecha =  (not fecha_desde) or (fecha_desde <= fecha_lista <= fecha_hasta)
+        filtro_estado = (not estado) or (estado in x)
 
+        if all([filtro_dni, filtro_fecha, filtro_estado]):
 
+            cheques_filtrados.append(x)
+
+    return cheques_filtrados
 
 
 def checkinput():
     
-    #La funcion checkinput() se encarga de checkear los Input values.
-    #La funcion devuelve True o False dependiendo en base del cumplimiento de condiciones.
+    #---------------------------------------------------------------------------------
+    # La funcion checkinput() se encarga de checkear el correcto ingreso de variables.
+    #   por parte del usuario.
+    #
+    # Input:
+    #   Empty.
+    #
+    # Ouput:
+    #   booleano: True o False segun las condiciones establecidas de ingreso.
+    #   diccionario: Contiene todos los campos ingresados por el usuario.
+    #----------------------------------------------------------------------------------
 
-    while len(sys.argv)<7:
-        sys.argv.append("")
+    argv = sys.argv
+    opts, args = getopt.getopt(argv[1:], "c:d:s:t:e:f:a")
+    diccionario = {}
+  
+    for letter, value in opts:
 
-    csvname, dni, output, type, status, date = sys.argv[1::]
+        if letter == "-c":
+            diccionario["csv_name"] = value
+        
+        elif letter == "-d":
+            diccionario["dni"]      = value.upper()
+        
+        elif letter == "-s":
+            diccionario["salida"]   = value.upper()
+        
+        elif letter == "-t":
+            diccionario["tipo"]     = value.upper()
+        
+        elif letter == "-e":
+            diccionario["estado"]   = value.upper()
+        
+        elif letter == "-f":
 
-    check_csv = csvname.split(".")[1] == "txt"
-    check_dni = str(dni).isnumeric()
-    check_output = output.upper() in ["PANTALLA", "SALIDA"]
-    check_type = type.upper() in ["EMITIDO", "DEPOSITADO"]
-    check_status = status.upper() in ["PENDIENTE", "APROBADO", "RECHAZADO",""]
-    check_date = True
+            inicio,fin = value.split(":")
+            diccionario["fecha_desde"] = datetime.strptime(inicio, '%d-%m-%Y').timestamp()
+            diccionario["fecha_hasta"] = datetime.strptime(fin, '%d-%m-%Y').timestamp()
+        
+    check_csv    = re.search(r".csv\b",diccionario.get("csv_name",""))
+    check_dni    = diccionario.get("dni","").isnumeric()
+    check_output = diccionario.get("salida","") in ["PANTALLA", "SALIDA"]
+    check_type   = diccionario.get("tipo","") in ["EMITIDO", "DEPOSITADO"]
+    check_status = diccionario.get("estado","") in ["PENDIENTE", "APROBADO", "RECHAZADO",""]
+    
+    if diccionario.get("fecha_hasta","")=="" and diccionario.get("fecha_desde","") == "":
 
-    return all([check_csv, check_dni, check_output, check_type, check_status, check_date])
+        check_date = True
+    
+    elif diccionario.get("fecha_desde","") < diccionario.get("fecha_hasta",""):
+
+        check_date = True
+
+    else :
+        check_date = False
+
+    return all([check_csv, check_dni, check_output, check_type, check_status, check_date]), diccionario
 
 
 def init():
     
+    #-----------------------------------------------------------------------
     # La funcion init() se encarga de inicializar el programa de busqueda.
+    #
+    # Input:
+    #   Empty.
+    #
+    # Ouput:
+    #   Empty.
+    #------------------------------------------------------------------------
 
-    if (checkinput()):
-
-        #Obtengo todas las variables de entrada
-        csvname, dni, output, type, status, date = sys.argv[1::]
+    check, diccionario = checkinput()
+    if (check):
 
         #Genero apertura del archivo
-        archivo = open(csvname)
+        archivo = open(diccionario["csv_name"])
         
         #Filtrado de la lista
         lista = archivo.readlines()
-        cheques = search_check(lista, dni, type, status, date)
+        cheques_filtrados = search_check(lista, diccionario)
+
+        print(cheques_filtrados)
 
         #Genero Output de la funcion.
         
     else:
         print("Error de Ingreso")
 
-
-
+#Inicializa el Proceso de busqueda
 init()
+
 
