ab0cb7e82fc174599fa2e7c87f3a1a01522deec0
jacc186 <jeschadee@gmail.com>
2022-07-16 17:56:19 -0300

    Cliente

--- Sprint5/Cliente.py
+++ Sprint5/Cliente.py
@@ -1,8 +1,9 @@
 from Sprint5.Direccion import Direccion
 from Cuenta import Cuenta
 class Cliente():
-    def __init__(self,numero,nombre,apellido,dni,tipo, direccion):
+    def __init__(self,numero,nombre,apellido,dni,tipo, Direccion):
         self.numero = numero
+        self.nombre = nombre
         self.apellido = apellido
         self.dni = dni
         self.tipo = tipo
@@ -13,22 +14,22 @@ class Cliente():
         self.puede_comprar_dolar = False
 
 class Classic(Cliente):
-    def __init__ (self,id,name,sName,dni):
-        Cliente.__init__(self,id,name,sName,dni, "Classic")
+    def __init__ (self,id,name,sName,dni, Direccion):
+        Cliente.__init__(self,id,name,sName,dni, "Classic", Direccion)
         self.puede_crear_chequera =  False
         self.puede_crear_tarjeta_credito = False
         self.puede_comprar_dolar = False        
 
 class Gold (Cliente):
-    def __init__(self, id, name, sName, dni,tipo):
-        Cliente.__init__(self,id,name,sName, dni, "Gold")
+    def __init__(self, id, name, sName, dni,tipo, Direccion):
+        Cliente.__init__(self,id,name,sName, dni, "Gold", Direccion)
         self.puede_crear_chequera =  True
         self.puede_crear_tarjeta_credito = True
         self.puede_comprar_dolar = True
 
 class Black (Cliente):
-    def __init__(self, id, name, sName, dni):
-        Cliente.__init__(self,id,name,sName, dni)
+    def __init__(self, id, name, sName, dni, Direccion):
+        Cliente.__init__(self,id,name,sName, dni, Direccion)
         self.puede_crear_chequera =  True
         self.puede_crear_tarjeta_credito = True
         self.puede_comprar_dolar = True
