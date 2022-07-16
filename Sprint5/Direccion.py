85a2e0deaa037e20ea4d05f275c9c5b21deea1d3
jacc186 <jeschadee@gmail.com>
2022-07-16 16:35:36 -0300

    Direccion

---
+++ Sprint5/Direccion.py
@@ -0,0 +1,7 @@
+class Direccion():
+    def __init__(self, calle, numero, ciudad, provincia, pais):
+        self.calle = calle
+        self. numero = numero
+        self.ciudad = ciudad
+        self.provincia = provincia
+        self.pais = pais
