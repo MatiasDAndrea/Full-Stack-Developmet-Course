6bccbec3f88d89d87783f9999cc774264c465015
MatiasDAndrea <105309938+MatiasDAndrea@users.noreply.github.com>
2022-07-16 18:37:10 -0300

    Razon.py Modulo Finalizado

--- Sprint5/Cliente.py
+++ Sprint5/Cliente.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import Cuenta
-
-
-class Cliente():
-    def __init__(self,id,name,sName,dni,tipe):
-        id=self.id
-        name=self.name
-        sName=self.sName
-        dni=self.dni
-        self.debitCard = 1
-        tipe = tipe if tipe != None else 'Classic'
-        self.Cuenta = Cuenta.Cuenta.__init__()
-    def puede_crear_chequera():
-        return bool
-    def puede_crear_tarjeta_credito():
-        return bool
-    def puede_comprar_dolar():
-        return bool
-
-class Gold (Cliente):
-    def __init__(self, id, name, sName, dni):
-            Cliente.__init__(self,id,name,sName, dni)
-            self.tipe= "GOLD"
-            self.creditCard = 1
-            self.dolarAccount= True
-            self.debit= 20000
-            self.cheq= True
-            self.comission= 0.5
-            self.maxTransf= 500000
-=======
 from Sprint5.Direccion import Direccion
 from Cuenta import Cuenta
 class Cliente():
@@ -43,7 +12,6 @@ class Cliente():
         self.puede_crear_chequera =  False
         self.puede_crear_tarjeta_credito = False
         self.puede_comprar_dolar = False
->>>>>>> 4931365f2686738f745c5ae9df9d0c5854220735
 
 class Classic(Cliente):
     def __init__ (self,id,name,sName,dni, Direccion):
