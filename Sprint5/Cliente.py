ea51ea1e699ef22f091c986a83fb9198879cd9e2
jacc186 <jeschadee@gmail.com>
2022-07-16 17:39:05 -0300

    ceunta y cliente

--- Sprint5/Cliente.py
+++ Sprint5/Cliente.py
@@ -1,53 +1,35 @@
-from abc import abstractclassmethod
-import Cuenta
-
-@abstractclassmethod
+from Sprint5.Direccion import Direccion
+from Cuenta import Cuenta
 class Cliente():
-    def __init__(self,id,name,sName,dni,tipe):
-        id=self.id
-        name=self.name
-        sName=self.sName
-        dni=self.dni
-        self.debitCard = 1
-        tipe = tipe if tipe != None else 'Classic'
-        self.Cuenta = Cuenta.Cuenta.__init__()
-    def puede_crear_chequera():
-        return bool
-    def puede_crear_tarjeta_credito():
-        return bool
-    def puede_comprar_dolar():
-        return bool
-
-class Gold (Cliente):
-    def __init__(self, id, name, sName, dni,tipe):
-            Cliente.__init__(self,id,name,sName, dni)
-            self.tipe= "GOLD"
-            self.creditCard = 1
-            self.dolarAccount= True
-            self.debit= 20000
-            self.cheq= True
-            self.comission= 0.5
-            self.maxTransf= 500000
+    def __init__(self,numero,nombre,apellido,dni,tipo, direccion):
+        self.numero = numero
+        self.apellido = apellido
+        self.dni = dni
+        self.tipo = tipo
+        self.direccion = Direccion(tipo)
+        self.cuenta = Cuenta(tipo)
+        self.puede_crear_chequera =  False
+        self.puede_crear_tarjeta_credito = False
+        self.puede_comprar_dolar = False
 
 class Classic(Cliente):
-    def __init__ (self,id,name,sName,dni,tipe):
-        Cliente.__init__(self,id,name,sName,dni)
-        self.tipe="CLASSIC"
-        self.creditCard = 0
-        self.dolarAccount= False
-        self.debit= 10000
-        self.cheq= False
-        self.comission= 1
-        self.maxTransf= 150000 
+    def __init__ (self,id,name,sName,dni):
+        Cliente.__init__(self,id,name,sName,dni, "Classic")
+        self.puede_crear_chequera =  False
+        self.puede_crear_tarjeta_credito = False
+        self.puede_comprar_dolar = False        
+
+class Gold (Cliente):
+    def __init__(self, id, name, sName, dni,tipo):
+        Cliente.__init__(self,id,name,sName, dni, "Gold")
+        self.puede_crear_chequera =  True
+        self.puede_crear_tarjeta_credito = True
+        self.puede_comprar_dolar = True
 
 class Black (Cliente):
     def __init__(self, id, name, sName, dni):
         Cliente.__init__(self,id,name,sName, dni)
-        self.tipe="BLACK"
-        self.creditCard = 1
-        self.dolarAccount= True
-        self.debit= 100000
-        self.cheq= 1
-        self.comission= 0
-        self.maxTransf= 0
+        self.puede_crear_chequera =  True
+        self.puede_crear_tarjeta_credito = True
+        self.puede_comprar_dolar = True
 
--- Sprint5/Cuenta.py
+++ Sprint5/Cuenta.py
@@ -1,4 +1,14 @@
 class Cuenta():
-    def __init__(self):
-    def __LimiteDiario__():
+    def __init__(self,tipo):
+        if(tipo == 'Classic')
+            self.total_tarjeta_debito = 1
+            self.total_tarjeta_credito = 0
+            self.debit= 10000
+            self.cheq= False
+            self.comision= 1
+            self.limite_extraccion_diario= 10000 
+            self.total_chequera_limite = 0
+        if(tipo == 'Gold')
+
+        
 
