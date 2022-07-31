f7a98ad4b3615c772296b183935798966a9748cf
MatiasDAndrea <105309938+MatiasDAndrea@users.noreply.github.com>
2022-07-31 18:41:59 -0300

    Avances en Problematica Cuatro

--- Sprint-6/4-CuartaProblematica.sql
+++ Sprint-6/4-CuartaProblematica.sql
@@ -18,4 +18,25 @@ from cliente C
     INNER JOIN Empleado E ON S.branch_id=E.branch_id
     GROUP by NombreSucursal,NombreCliente ;
 
+
 --3 
+
+SELECT 
+    COUNT(*) as Cantidad,
+    branch_name as NombreSucursal
+FROM Tarjetas T 
+    INNER JOIN cliente C on T.customer_id = C.customer_id
+    INNER JOIN sucursal S on S.branch_id = C.branch_id
+WHERE Tarjeta_tipo = "CREDITO"
+GROUP BY NombreSucursal
+ORDER BY Cantidad;
+
+-- 4 
+SELECT 
+    avg(loan_total) as PromedioPrestamos,
+    branch_name as NombreSucursal
+FROM cliente C
+    INNER JOIN sucursal S on S.branch_id = C.branch_id  
+    INNER JOIN prestamo P on P.customer_id = C.customer_id
+GROUP BY NombreSucursal
+ORDER BY PromedioPrestamos;
