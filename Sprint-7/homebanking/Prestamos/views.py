e8eb005c2c953cbcddb44ef18fb12a3022dd808d
MatiasDAndrea <105309938+MatiasDAndrea@users.noreply.github.com>
2022-08-14 01:19:54 -0300

    Creando Prestamos y metodo de POST

--- Sprint-7/homebanking/Prestamos/views.py
+++ Sprint-7/homebanking/Prestamos/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.urls import reverse
 
 # Create your views here.
+
+class PrestamoPackage:
+    
+
+    def log_page(request):
+        
+        return render(request,"Prestamos/Prestamos.html")
+
+    def mensaje(request):
+
+        if request.method == 'POST':
+            monto         = request.POST.get('Monto',0)
+            tipo_prestamo = request.POST.get("tipoprestamo","").lower()
+
+            ###############################
+            # Checkeo parametros ingresados.
+            opcion_prestamo = ["hipotecario","personal","prendario"]
+            monto_bool = monto > 0
+            tipo_prestamo_bool = tipo_prestamo in opcion_prestamo
+            
+            ################################
+            # Checkeo segun tipo de cliente.
+
+
+
+
+        return redirect(reverse('prestamos'))
