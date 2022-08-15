8a42cd452b493afa748c5da775fc0a8e529835e4
MatiasDAndrea <105309938+MatiasDAndrea@users.noreply.github.com>
2022-08-14 22:56:52 -0300

    Se añade goCripto al coctail

--- Sprint-7/homebanking/Cuentas/urls.py
+++ Sprint-7/homebanking/Cuentas/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('',views.CuentasPackage.cuenta,name="cuenta")
+    path('',views.CuentasPackage.cuenta,name="cuenta"),
+    path('goCripto',views.CuentasPackage.gocripto_render,name="gocripto")
 ]
