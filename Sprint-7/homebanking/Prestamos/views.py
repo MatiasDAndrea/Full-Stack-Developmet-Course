0f8e7a8028c6ec6a61e3fae85db846fc1f542a66
MatiasDAndrea <105309938+MatiasDAndrea@users.noreply.github.com>
2022-08-14 22:43:46 -0300

    Detalles Problematica3

--- Sprint-7/homebanking/Prestamos/views.py
+++ Sprint-7/homebanking/Prestamos/views.py
@@ -76,7 +76,7 @@ class PrestamoPackage:
                 # Update del balance de la cuenta.
                 print(cuenta,type(cuenta))
                 saldo_inicial = Cuenta.objects.get(account_id = int(cuenta)).balance
-                Cuenta.objects.get(account_id = cuenta).update(balance=saldo_inicial+monto)
+                Cuenta.objects.filter(account_id = cuenta).update(balance=saldo_inicial+monto)
 
                 ######################################
                 # Enviamos el mensaje correspondiente.
